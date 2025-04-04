7436af7723ffa73a1871cb67d8559aa581bd3f06
mordecai <heitor_emmerich@estudante.sesisenai.org.br>
2025-04-04 08:15:43 -0300

    ADD - Redirecionador de paginas, parte do <p> e umas correções no desing

---
+++ script.js
@@ -0,0 +1,7 @@
+function direcaoJs(){
+    window.location = 'file:///C:/Users/heitor_emmerich/Desktop/resumao_site/paginas/pagina_js.html'
+}
+
+function direcaoHtml(){
+    window.location = 'file:///C:/Users/heitor_emmerich/Desktop/resumao_site/index.html'
+}
